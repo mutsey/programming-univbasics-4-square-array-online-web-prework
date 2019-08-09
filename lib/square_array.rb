0635634c560eb908fe81f9e8c06cbf90f4bfa00e
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-09 04:49:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
+def while= square_array(array)
 numbers=[4,8,10]
 
 square_array(numbers)
 #=>[16,64,100])
 
-new_numbers = [3,6,9]
+while= new_numbers = [3,6,9]
 
 square_array(new_numbers)
 #=> [9,36,81]
