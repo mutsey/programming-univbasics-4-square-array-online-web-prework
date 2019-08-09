7880394fec2e11dbaec679c94d716130cc39b0b3
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-09 04:49:22 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def while= square_array(array)
+numbers=[4,8,10]
+
+square_array(numbers)
+#=>[16,64,100])
+
+while= new_numbers = [3,6,9]
+
+square_array(new_numbers)
+#=> [9,36,81]
 end
